2876e4b71aa867cd1a1e8ae9488f99008af6c154
Jerel R Hass <agies1@hotmail.com>
2014-04-03 01:42:13 -0400

    added ball

---
+++ Assets/Scripts/TapToBounce.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class TapToBounce : MonoBehaviour {
+    private Rigidbody ballRigidBody;
+
+    // Use this for initialization
+	void Start ()
+	{
+        ballRigidBody = rigidbody;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.touchCount > 0)
+	    {
+            Touch();
+	    }
+        if (Input.GetButton("Fire1"))
+        {
+            Touch();
+        }
+	}
+
+    private void Touch()
+    {
+        Debug.Log("Touched!");
+        ballRigidBody.AddForce(0, 1, 0, ForceMode.VelocityChange);
+    }
+}
